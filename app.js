0a796e3a9e6780358ad45c592a4fd6f87629d25f
Munna Hossain <mayinuddin.munna@gmail.com>
2021-02-08 18:29:59 +0600

    make sure search bar is workubg

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
+
 const findFoods = () => {
+    
     const searchFood = document.getElementById('search-field').value;
-    const url = ` https://www.themealdb.com/api/json/v1/1/list.php?i=list`
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchFood}`
     fetch(url)
         .then(res => res.json())
         .then(data => displayFoods(data.meals));
 }
 const displayFoods = foods => {
-    const foodContainer = document.getElementById('food-contaiber');
+    const foodContainer = document.getElementById('food-container');
 
     foods.forEach(food => {
         console.log(food);
         const foodDiv = document.createElement('div');
         foodDiv.className = 'col mt-5'
         foodDiv.innerHTML = `
-        <div class="card card text-center shadow rounded">
-            <img src="images/menu1.png" class="card-img-top " alt="...">
+        <div class="text-center shadow">
             <div class="card-body">
-                <h5 class="card-title">${food.strIngredient}</h5>
+                <h5 class="card-title">${food.strMeal}</h5>
+                <img src="${food.strMealThumb}" class="img-size card-img-top " alt="...">
+                <div class="mt-2">
+                <button onclick="getFood('${food.strMeal}')" class="btn btn-outline-success" type="submit">Click</button>
+                </div>
+
             </div>
         </div>
         `;
         foodContainer.appendChild(foodDiv);
-});
-
+    })
 }
+
+// const getFood = catagories => {
+//     const url = `https://www.themealdb.com/api/json/v1/1/categories.php`
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => displayFood(data.categories));
+// }
+// const displayFood = food => {
+//     const foodsDiv = document.getElementById('food-categories');
+//     foodsDiv.innerText = food;
+// }
