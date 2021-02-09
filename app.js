7f4618e780f6ea48d3ab8752dca8f9c589d79123
Munna Hossain <mayinuddin.munna@gmail.com>
2021-02-09 21:58:52 +0600

    Error message setup

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const findFoods = () => {
+    document.getElementById('error-message').innerText = "";
 
     const searchFood = document.getElementById('search-field').value;
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchFood}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayFoods(data.meals));
+        .then(data => displayFoods(data.meals))
+        .catch(error => displayError('Something Went Wrong!! Please try again later!'));
 }
 const displayFoods = foods => {
     const foodContainer = document.getElementById('food-container');
@@ -12,13 +14,13 @@ const displayFoods = foods => {
     foods.forEach(food => {
         console.log(food);
         const foodDiv = document.createElement('div');
-        
+
         foodDiv.className = 'col mt-5'
         foodDiv.innerHTML = `
         
         <div class="text-center shadow">
             <div class="card-body card-body2">
-                <button onclick="getFood('${food.strCategory}')" class="btn" type="submit">
+                <button onclick="getFood(${food.idMeal})" class="btn" type="submit">
                 <img src="${food.strMealThumb}" class="img-size card-img-top " alt="...">
                 <h5 class="card-title">${food.strMeal}</h5>
                 </button>
@@ -28,15 +30,29 @@ const displayFoods = foods => {
         foodContainer.appendChild(foodDiv);
     });
 }
-const getFood = catagories => {
-    console.log(catagories);
-    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${catagories}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayFood(data.catagories));
+const getFood = async (idMeal) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+        
+        displayFood(data.strIngredient1);
 
+        // .then(res => res.json())
+        // .then(data => displayFood(data.catagories));
+    }
+    catch (error) {
+        displayError('Sorry! Please try again later!!!')
+
+    }
 }
 const displayFood = food => {
     const foodsDiv = document.getElementById('food-categories');
     foodsDiv.innerText = food;
+}
+const displayError = error => {
+    const errorTag = document.getElementById('error-message');
+
+    errorTag.innerText = error;
 }
